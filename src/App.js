15460be85d429f8eca7fb869722add512bacc078
github username <a_schittone@yahoo.com>
2017-12-12 04:33:12 -0500

    remove some modules

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <center>
           <div >
             <header >
-              <h1>Please enter your candidate ID to search for your communcation and coding score percentiles.</h1>
+              <h1>Please enter your candidate ID to search for your communication and coding score percentiles.</h1>
               <h2>(compared to others with the same position, at similar companies)</h2>
             </header>
             <br></br>
